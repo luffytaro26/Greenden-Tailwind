8a426a961c19b5b65dcab809050b0ba1c6edcc38
luffytaro26 <dhonikarthick26@gmail.com>
2025-05-13 11:14:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var sidenav = document.getElementById("sidenav");
+var menuicon = document.getElementById("menuicon");
+var x= document.getElementById("x");
+menuicon.addEventListener("click", function() {
+    sidenav.style.right=0});
+x.addEventListener("click", function() {
+    sidenav.style.right="-50%";
+});
